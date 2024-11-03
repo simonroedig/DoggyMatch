45246e29eb9944b8f7a3b96d2f43367a2adc9c0c
simonroedig <simon.roedig@web.de>
2024-11-04 00:18:31 +0100

    community page, stop from reloading when closing profile

--- lib/toggles/friends_receivedreq_sentreq_toggle.dart
+++ lib/toggles/friends_receivedreq_sentreq_toggle.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:doggymatch_flutter/main/colors.dart';
 
@@ -7,7 +9,6 @@ class FriendsReceivedReqSentReqToggle extends StatefulWidget {
   const FriendsReceivedReqSentReqToggle({super.key, required this.onToggle});
 
   @override
-  // ignore: library_private_types_in_public_api
   _FriendsReceivedReqSentReqToggleState createState() =>
       _FriendsReceivedReqSentReqToggleState();
 }
@@ -16,29 +17,29 @@ class _FriendsReceivedReqSentReqToggleState
     extends State<FriendsReceivedReqSentReqToggle> {
   int currentState = 0; // 0 = Friends, 1 = Received, 2 = Sent
 
-  void toggleSwitch() {
+  void toggleSwitch(int index) {
     setState(() {
-      currentState = (currentState + 1) % 3; // Cycle through 0 -> 1 -> 2 -> 0
+      currentState = index;
     });
     widget.onToggle(currentState);
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: toggleSwitch,
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        height: 40,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          border: Border.all(color: AppColors.customBlack, width: 3),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Friends state
-            Expanded(
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        border: Border.all(color: AppColors.customBlack, width: 3),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Friends state
+          Expanded(
+            child: GestureDetector(
+              onTap: () => toggleSwitch(0),
               child: Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
@@ -52,30 +53,37 @@ class _FriendsReceivedReqSentReqToggleState
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.people_alt_rounded,
-                        size: 24,
-                        color: currentState == 0
-                            ? AppColors.bg
-                            : AppColors.customBlack),
+                    Icon(
+                      Icons.people_alt_rounded,
+                      size: 24,
+                      color: currentState == 0
+                          ? AppColors.bg
+                          : AppColors.customBlack,
+                    ),
                     const SizedBox(width: 4),
                     Transform.translate(
-                      offset: const Offset(-6, -3), // Adjust icon position
-                      child: Icon(Icons.check_rounded,
-                          size: 16,
-                          color: currentState == 0
-                              ? AppColors.bg
-                              : AppColors.customBlack),
+                      offset: const Offset(-6, -3),
+                      child: Icon(
+                        Icons.check_rounded,
+                        size: 16,
+                        color: currentState == 0
+                            ? AppColors.bg
+                            : AppColors.customBlack,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-            Container(
-              width: 2,
-              color: AppColors.customBlack,
-            ),
-            // Received state
-            Expanded(
+          ),
+          Container(
+            width: 2,
+            color: AppColors.customBlack,
+          ),
+          // Received state
+          Expanded(
+            child: GestureDetector(
+              onTap: () => toggleSwitch(1),
               child: Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
@@ -85,30 +93,37 @@ class _FriendsReceivedReqSentReqToggleState
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.people_alt_rounded,
-                        size: 24,
-                        color: currentState == 1
-                            ? AppColors.bg
-                            : AppColors.customBlack),
+                    Icon(
+                      Icons.people_alt_rounded,
+                      size: 24,
+                      color: currentState == 1
+                          ? AppColors.bg
+                          : AppColors.customBlack,
+                    ),
                     const SizedBox(width: 4),
                     Transform.translate(
-                      offset: const Offset(-6, -3), // Adjust icon position
-                      child: Icon(Icons.call_received_rounded,
-                          size: 16,
-                          color: currentState == 1
-                              ? AppColors.bg
-                              : AppColors.customBlack),
+                      offset: const Offset(-6, -3),
+                      child: Icon(
+                        Icons.call_received_rounded,
+                        size: 16,
+                        color: currentState == 1
+                            ? AppColors.bg
+                            : AppColors.customBlack,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-            Container(
-              width: 2,
-              color: AppColors.customBlack,
-            ),
-            // Sent state
-            Expanded(
+          ),
+          Container(
+            width: 2,
+            color: AppColors.customBlack,
+          ),
+          // Sent state
+          Expanded(
+            child: GestureDetector(
+              onTap: () => toggleSwitch(2),
               child: Container(
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
@@ -122,26 +137,30 @@ class _FriendsReceivedReqSentReqToggleState
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.people_alt_rounded,
-                        size: 24,
-                        color: currentState == 2
-                            ? AppColors.bg
-                            : AppColors.customBlack),
+                    Icon(
+                      Icons.people_alt_rounded,
+                      size: 24,
+                      color: currentState == 2
+                          ? AppColors.bg
+                          : AppColors.customBlack,
+                    ),
                     const SizedBox(width: 4),
                     Transform.translate(
-                      offset: const Offset(-6, -3), // Adjust icon position
-                      child: Icon(Icons.call_made_rounded,
-                          size: 16,
-                          color: currentState == 2
-                              ? AppColors.bg
-                              : AppColors.customBlack),
+                      offset: const Offset(-6, -3),
+                      child: Icon(
+                        Icons.call_made_rounded,
+                        size: 16,
+                        color: currentState == 2
+                            ? AppColors.bg
+                            : AppColors.customBlack,
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
